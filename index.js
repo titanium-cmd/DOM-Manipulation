4d0ca1c1b605b5622456de97126983fc798fb27f
titanium-cmd <nkansahisaac41@gmail.com>
2022-10-19 07:00:45 +0000

    created a function to populate table header

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
-const createListItem = (name) => {
-    const li = document.createElement('li');
-    const a = document.createElement('a');
-    a.href = ""
-    li.appendChild(a);
-    li.textContent = name;
-    return li;
-}
+const BASE_URL = 'https://jsonplaceholder.typicode.com';
 
-const loader = ({ state = 'show' }) => { 
+const loader = ({ state = 'show' }) => {
     const p = document.querySelector('#loader');
     p.textContent = 'Loading...';
     p.style.display = state === 'hide' ? 'none' : 'block';
@@ -16,12 +9,37 @@ const loader = ({ state = 'show' }) => {
 const getUsers = async () => {
     try {
         loader({ state: 'show' });
-        const res = await fetch('https://jsonplaceholder.typicode.com/users');
+        const res = await fetch(`${BASE_URL}/users`);
         const users = await res.json();
         return users;
     } catch (error) {
         console.log('error::', error);
-        alert("Error fetching users. Kindly check your internet connection and try again.");
+        // alert("Error fetching users. Kindly check your internet connection and try again.");
+    } finally {
+        loader({ state: 'hide' });
+    }
+}
+
+const addHeadersToTable = ({ headers, table }) => { 
+    if (headers && Array.isArray(headers) && table) { 
+        const tableHead = table.createTHead();        
+        const tableHeadRow = tableHead.insertRow(0);
+        for (let i = 0; i < headers.length; i++) {
+            const header = headers[i];
+            tableHeadRow.insertCell(i).innerHTML = `<b>${header}</b>`;
+        }
+    }
+}
+
+const getAllPostByUserId = async ({user_id}) => {
+    try {
+        loader({ state: 'show' });
+        const res = await fetch(`${BASE_URL}/users/${user_id}/posts`);
+        const posts = await res.json();
+        return posts;
+    } catch (error) {
+        console.log('error::', error);
+        // alert("Error fetching user posts. Kindly check your internet connection and try again.");
     } finally {
         loader({ state: 'hide' });
     }
@@ -31,14 +49,35 @@ const getUsers = async () => {
     const users = await getUsers();
     if (users) {
         const usersTable = window.document.querySelector('table');
-        // usersTable.createTHead
+
+        // adding headers to users table
+        addHeadersToTable({
+            table: usersTable,
+            headers: ['ID', 'Name', 'Email Address', 'Website', 'Phone Number', 'Action']
+        });
+
+        //Insert list of users fetched from endpoint to table rows
         for (let i = 0; i < users.length; i++) {
             const user = users[i];
-            const row = usersTable.insertRow(i);   
+            const row = usersTable.insertRow(i+1); //First row of table is occupied by THead so start insert from row 2
+            const websiteLink = document.createElement('a');
+            websiteLink.href = user.website;
+            websiteLink.target = '_blank';
+            websiteLink.innerHTML = user.website;
             row.insertCell(0).appendChild(document.createTextNode(user.id))
             row.insertCell(1).appendChild(document.createTextNode(user.name))
             row.insertCell(2).appendChild(document.createTextNode(user.email))
+            row.insertCell(3).appendChild(websiteLink);
+            row.insertCell(4).appendChild(document.createTextNode(user.phone))
+
+            const action = document.createElement('a');
+            action.href = '#';
+            action.innerHTML = '<p>View Posts</p>';
+            row.insertCell(5).appendChild(action);
+            action.onclick = async () => {
+                const posts = await getAllPostByUserId(user.id);
+                console.log();
+            }
         }
     }
-    console.log('users::: ', users);
 })()
